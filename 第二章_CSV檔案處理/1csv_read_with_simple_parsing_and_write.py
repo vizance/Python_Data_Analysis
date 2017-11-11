ca5aa12bd036882c93f09be142d105e01b28d139
vizance <muhenry608@gmail.com>
2017-11-11 10:26:43 +0800

    使用基本讀取來讀寫csv模組(參考用，比較沒效率)

---
+++ 第二章_CSV檔案處理/1csv_read_with_simple_parsing_and_write.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+"""
+Created on Tue Sep 19 10:01:30 2017
+
+@author: vizance
+"""
+#===練習csv讀取===
+##使用simple python方法
+import sys
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(input_file, 'r', newline='') as filereader:
+    with open(output_file, 'w', newline='') as filewriter:
+        header = filereader.readline()
+        header = header.strip()
+        header_list = header.split(',')
+        print(header_list)
+        filewriter.write(','.join(map(str,header_list))+'\n')
+        for row in filereader:
+            row = row.strip()
+            row_list = row.split(',')
+            print(row_list)
+            filewriter.write(','.join(map(str,row_list))+'\n')

1e42291fa334b7663c85169793f7afab4d2c613f
vizance <muhenry608@gmail.com>
2017-11-20 09:34:19 +0800

    處理單張工作表(將單張worksheet讀入到新檔案)

---
+++ 第三章_Excel檔案處理/2excel_parsing_and_write.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#!usr/bin/env python3
+#處理單張工作表(將單張worksheet讀入到新檔案)
+"""
+Created on Mon Oct  2 09:48:52 2017
+
+@author: vizance
+"""
+import sys
+from xlrd import open_workbook
+from xlwt import Workbook
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+output_workbook = Workbook()#寫出工作簿
+output_worksheet = output_workbook.add_sheet('jan_2013_output')#寫出工作表
+with open_workbook(input_file) as workbook:#讀取input_file
+    worksheet = workbook.sheet_by_name('january_2013')
+    for row_index in range(worksheet.nrows):#先讀列
+        for col_index in range(worksheet.ncols):#再讀欄
+            output_worksheet.write(row_index, col_index, worksheet.cell_value(row_index, col_index))
+output_workbook.save(output_file)

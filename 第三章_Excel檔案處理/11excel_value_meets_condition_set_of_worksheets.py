04d6635d8de1d0643c62e55571954678573d014a
vizance <muhenry608@gmail.com>
2017-11-23 11:21:28 +0800

    讀取Excel活頁簿中特定幾張工作表(上一個範例檔是讀取所有的worksheets，此節只找第1、2張worksheets，sales>1900)

---
+++ 第三章_Excel檔案處理/11excel_value_meets_condition_set_of_worksheets.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#!/ust/bin/env python3
+#讀取Excel活頁簿中特定幾張工作表(上一個範例檔是讀取所有的worksheets，此節只找第1、2張worksheets，sales>1900)
+"""
+Created on Thu Oct  5 09:10:10 2017
+
+@author: vizance
+"""
+import sys
+from datetime import date
+from xlrd import open_workbook, xldate_as_tuple
+from xlwt import Workbook
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+output_workbook = Workbook()
+output_worksheet = output_workbook.add_sheet('set_of_worksheets')
+my_sheets = [0,1]
+threshold = 1900.0
+sale_column_index = 3
+first_worksheet = True
+with open_workbook(input_file) as workbook:
+    data = []
+    for sheet_index in range(workbook.nsheets):#判斷目前是在第幾張worksheet
+        if sheet_index in my_sheets:
+            worksheet = workbook.sheet_by_index(sheet_index)
+            if first_worksheet:
+                header_row = worksheet.row_values(0)
+                data.append(header_row)
+                first_worksheet = False
+            for row_index in range(1, worksheet.nrows):
+                row_list = []
+                sale_amount = worksheet.cell_value(row_index, sale_column_index)
+                if sale_amount > threshold:
+                    for column_index in range(worksheet.ncols):
+                        cell_value = worksheet.cell_value(row_index, column_index)
+                        cell_type = worksheet.cell_type(row_index, column_index)
+                        if cell_type ==3:
+                            date_cell = xldate_as_tuple(cell_value, workbook.datemode)
+                            date_cell = date(*date_cell[0:3]).strftime('%m/%d/%Y')
+                            row_list.append(date_cell)
+                        else:
+                            row_list.append(cell_value)
+                if row_list:
+                    data.append(row_list)
+    for list_index, output_list in enumerate(data):
+        for element_index, element in enumerate(output_list):
+            output_worksheet.write(list_index, element_index, element)
+output_workbook.save(output_file)
